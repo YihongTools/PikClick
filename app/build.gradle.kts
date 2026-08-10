a8142e5735aac17db5f9f33aaa2428fff49ac215
Yihong Cho <yihongcho@msn.com>
2026-08-10 13:49:37 +0800

    chore: prepare PikClick v2.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.pikclick.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.1.1"
+        versionCode = 23
+        versionName = "2.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
